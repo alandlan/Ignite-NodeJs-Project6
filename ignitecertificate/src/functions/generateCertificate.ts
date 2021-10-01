746e3503035b19d2061547a531e465f8613cd422
Alan Martins <alanhorcrux@gmail.com>
2021-10-01 15:39:15 -0300

    Exibindo Certificado part 1

--- ignitecertificate/src/functions/generateCertificate.ts
+++ ignitecertificate/src/functions/generateCertificate.ts
@@ -1,9 +1,32 @@
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import handlebars from "handlebars";
+import fs from "fs";
+import dayjs from "dayjs"
+
+
 import { document } from "../utils/dynamodbClient"
 
 interface ICreateCertificate {
     id: string;
     name: string;
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    date: string;
+    medal: string;
+}
+
+const compile = async function(data: ITemplate){
+    const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+    const html = fs.readFileSync(filePath, "utf-8");
+
+    return handlebars.compile(html)(data);
 }
 
 export const handle = async (event) => {
@@ -18,6 +41,21 @@ export const handle = async (event) => {
             grade
         }
     }).promise();
+
+    const medalPath = path.join(process.cwd(),"src","templates","selo.png");
+    const medal = fs.readFileSync(medalPath,"base64");
+
+    const data: ITemplate = {
+        date: dayjs().format("DD/MM/YYYY"),
+        grade,
+        name,
+        id,
+        medal
+    }
+
+    const content = await compile(data);
+
+
 
     return {
         statusCode: 201,
